557cc131e93c5104ff1ab150e3a3fbd5f0b6f3cd
duc-lq <lequangduc91@gmail.com>
2023-08-11 23:14:48 +0700

    Add Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ServerComponent } from './server/server.component';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './alert/successAlert/successAlert.component';
+import { WarningAlertComponent } from './alert/warningAlert/warningAlert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
